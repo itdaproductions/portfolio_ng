54081486f33cc925eee9f91fb1d58fa94c5c44a3
Adriano Andrade <allonan.619@gmail.com>
2023-01-28 14:59:33 +0000

    feat<portfolio>:
    Added new base module for the main content;

---
+++ src/app/content/portfolio/portfolio.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PortfolioRoutingModule } from './portfolio.routing';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PortfolioRoutingModule],
+})
+export class PortfolioModule {}
