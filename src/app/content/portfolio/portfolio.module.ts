057d165301559587e41aba82b4769ca28b9ad1a3
Adriano Andrade <allonan.619@gmail.com>
2023-01-29 18:19:58 +0000

    feat<portfolio>:
    Added routing to portfolio component;
    Added translations for portfolio hero;

--- src/app/content/portfolio/portfolio.module.ts
+++ src/app/content/portfolio/portfolio.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PortfolioRoutingModule } from './portfolio.routing';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PortfolioRoutingModule],
+  imports: [CommonModule, PortfolioRoutingModule, SharedModule],
 })
 export class PortfolioModule {}
