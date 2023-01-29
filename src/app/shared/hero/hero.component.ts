0f64315158eb72b03a7f730249b594305bd4c6c2
Adriano Andrade <allonan.619@gmail.com>
2023-01-29 18:20:57 +0000

    feat<hero component>:
    Added title, sub title and description inputs for hero component;
    Added optional button label;

--- src/app/shared/hero/hero.component.ts
+++ src/app/shared/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() subTitle: string = '';
+  @Input() description: string = '';
+  @Input() buttonLabel: string = '';
+
   constructor() {}
 
   ngOnInit(): void {}
