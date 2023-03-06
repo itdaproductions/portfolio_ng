843b2a8cdf78b117adb3b01f437e946621788af0
adrianooandrade <allonan.619@gmail.com>
2023-03-06 11:32:02 +0000

    feat<media & portfolio components>
    Added cards for categories in portfolio;

--- src/app/content/portfolio/portfolio.component.ts
+++ src/app/content/portfolio/portfolio.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Media } from 'src/app/shared/media/media.component';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
+  @Input() title: string = 'Categories';
+  @Input() medias: Media[] = [
+    { title: 'Design', description: 'test', buttonLabel: 'Check it out' },
+    { title: 'Digital', description: 'test', buttonLabel: 'Check it out' },
+    { title: 'Tradicional', description: 'test', buttonLabel: 'Check it out' },
+    { title: 'Escultura', description: 'test', buttonLabel: 'Check it out' },
+    { title: 'Music', description: 'test', buttonLabel: 'Check it out' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/media/media.component.ts
+++ src/app/shared/media/media.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+export interface Media {
+  title: string;
+  description: string;
+  buttonLabel?: string;
+}
+
 @Component({
   selector: 'app-media',
   templateUrl: './media.component.html',
@@ -8,7 +14,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MediaComponent implements OnInit {
   @Input() title: string = '';
   @Input() description: string = '';
-  @Input() buttonLabel: string = '';
+  @Input() buttonLabel: string | undefined = '';
 
   constructor() {}
 
