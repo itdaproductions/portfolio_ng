d45e1c26cd849ab2480a5ab15fa02228949f84d6
adrianooandrade <allonan.619@gmail.com>
2023-05-30 12:07:35 +0100

    feat<footer component>:
    Using the new socials component;

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SocialsData } from '../socials/socials.component';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  developer: string = 'Adriano Abreu Andrade';
+  developerRole: string = 'Software Engineer';
+  socialsData: SocialsData[] = [
+    {
+      type: 'linkedin',
+      link: 'https://www.linkedin.com/in/adriano-abreu-andrade/',
+    },
+    {
+      type: 'github',
+      link: 'https://github.com/adrianooandrade',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
