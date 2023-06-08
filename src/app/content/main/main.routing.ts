dd504c9d8e4e24f42ffd95730577940e0dc905d8
adrianooandrade <allonan.619@gmail.com>
2023-06-08 22:16:25 +0100

    feat<main-routing>: Removed hardcoded paths

--- src/app/content/main/main.routing.ts
+++ src/app/content/main/main.routing.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
-import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { ContactsComponent } from '../contacts/contacts.component';
 
 const routes: Routes = [
@@ -11,30 +10,6 @@ const routes: Routes = [
   },
   {
     path: 'portfolio',
-    component: PortfolioComponent,
-  },
-  {
-    path: 'portfolio/design',
-    loadChildren: () =>
-      import('./gallery/gallery.module').then((m) => m.GalleryModule),
-  },
-  {
-    path: 'portfolio/digital',
-    loadChildren: () =>
-      import('./gallery/gallery.module').then((m) => m.GalleryModule),
-  },
-  {
-    path: 'portfolio/tradicional',
-    loadChildren: () =>
-      import('./gallery/gallery.module').then((m) => m.GalleryModule),
-  },
-  {
-    path: 'portfolio/sculpture',
-    loadChildren: () =>
-      import('./gallery/gallery.module').then((m) => m.GalleryModule),
-  },
-  {
-    path: 'portfolio/music',
     loadChildren: () =>
       import('./gallery/gallery.module').then((m) => m.GalleryModule),
   },
